72638c0c52d1f651c02a598b039e1cd3ddd1f8d0
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-07-08 21:47:25 +0300

    isp-finished

--- interface_segregation_principle.php
+++ interface_segregation_principle.php
@@ -9,3 +9,84 @@
  *
  *  - All descriptions and comments created by ChatGPT and GitHub Copilot
  */
+
+/**
+ * Bad Example
+ */
+interface Machine{
+    public function print();
+    public function scan();
+    public function copy();
+}
+
+class MultiFunctionPrinter implements Machine{
+    public function print(): void
+    {
+        // print
+    }
+
+    public function scan(): void
+    {
+        // scan
+    }
+
+    public function copy(): void
+    {
+        // fax
+    }
+}
+
+/**
+ * Good Example
+ */
+interface Printer{
+    public function print();
+}
+
+interface Scanner{
+    public function scan();
+}
+
+interface Copier{
+    public function copy();
+}
+
+class LaserPrinter implements Printer{
+    public function print(): void
+    {
+        // print
+    }
+}
+
+class FlatbedScanner implements Scanner{
+    public function scan(): void
+    {
+        // scan
+    }
+}
+
+class PhotoCopier implements Printer, Scanner, Copier{
+    public function print(): void
+    {
+        // print
+    }
+
+    public function scan(): void
+    {
+        // scan
+    }
+
+    public function copy(): void
+    {
+        // copy
+    }
+}
+
+/**
+ * The interface segregation principle states that clients should not be forced to
+ * implement interfaces they don't use. Instead of one fat interface many small
+ * interfaces are preferred based on groups of methods, each one serving one
+ * submodule.
+ */
+
+
